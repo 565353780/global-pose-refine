9e6e100f2b198062cab1815dfeba3a2b5917ca11
chLi <565353780@qq.com>
2022-12-08 15:07:57 +0800

    use lower lr for refine model

--- global_pose_refine/Module/trainer.py
+++ global_pose_refine/Module/trainer.py
@@ -28,8 +28,8 @@ class Trainer(object):
 
     def __init__(self):
         self.batch_size = 1
-        self.lr = 1e-4
-        self.weight_decay = 1e-4
+        self.lr = 1e-5
+        self.weight_decay = 1e-5
         self.decay_step = 21
         self.lr_decay = 0.76
         self.lowest_decay = 0.02
