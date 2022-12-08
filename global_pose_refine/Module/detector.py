71d47e15c2fe0a0264fe8650e25c3f16c7c1ae7c
chLi <565353780@qq.com>
2022-12-09 00:13:32 +0800

    return np.ndarray as default

--- global_pose_refine/Module/detector.py
+++ global_pose_refine/Module/detector.py
@@ -9,7 +9,7 @@ import open3d as o3d
 import torch
 from tqdm import tqdm
 
-from global_pose_refine.Method.device import toCuda
+from global_pose_refine.Method.device import toCpu, toCuda, toNumpy
 from global_pose_refine.Model.gcnn.gcnn import GCNN
 
 
@@ -44,29 +44,34 @@ class Detector(object):
         floor_num = 1
         object_num = trans_object_obb.shape[0]
 
-        data['inputs']['floor_position'] = data['inputs'][
-            'floor_position'].reshape(1, floor_num, -1)
-        data['inputs']['floor_normal'] = data['inputs'][
-            'floor_normal'].reshape(1, floor_num, -1)
-        data['inputs']['floor_z_value'] = data['inputs'][
-            'floor_z_value'].reshape(1, floor_num, -1)
+        data['inputs']['floor_position'] = data['inputs']['floor_position'].to(
+            torch.float32).reshape(1, floor_num, -1)
+        data['inputs']['floor_normal'] = data['inputs']['floor_normal'].to(
+            torch.float32).reshape(1, floor_num, -1)
+        data['inputs']['floor_z_value'] = data['inputs']['floor_z_value'].to(
+            torch.float32).reshape(1, floor_num, -1)
 
-        data['inputs']['wall_position'] = data['inputs'][
-            'wall_position'].reshape(1, wall_num, -1)
-        data['inputs']['wall_normal'] = data['inputs']['wall_normal'].reshape(
-            1, wall_num, -1)
+        data['inputs']['wall_position'] = data['inputs']['wall_position'].to(
+            torch.float32).reshape(1, wall_num, -1)
+        data['inputs']['wall_normal'] = data['inputs']['wall_normal'].to(
+            torch.float32).reshape(1, wall_num, -1)
 
         data['inputs']['trans_object_obb'] = data['inputs'][
-            'trans_object_obb'].reshape(1, object_num, -1)
+            'trans_object_obb'].to(torch.float32).reshape(1, object_num, -1)
         data['inputs']['trans_object_abb'] = data['inputs'][
-            'trans_object_abb'].reshape(1, object_num, -1)
+            'trans_object_abb'].to(torch.float32).reshape(1, object_num, -1)
         data['inputs']['trans_object_obb_center'] = data['inputs'][
-            'trans_object_obb_center'].reshape(1, object_num, -1)
+            'trans_object_obb_center'].to(torch.float32).reshape(
+                1, object_num, -1)
 
         data['inputs']['trans_object_obb_center_dist'] = data['inputs'][
-            'trans_object_obb_center_dist'].reshape(1, -1, 1)
+            'trans_object_obb_center_dist'].to(torch.float32).reshape(
+                1, -1, 1)
         data['inputs']['trans_object_abb_eiou'] = data['inputs'][
-            'trans_object_abb_eiou'].reshape(1, -1, 1)
+            'trans_object_abb_eiou'].to(torch.float32).reshape(1, -1, 1)
 
         data = self.model(data)
+
+        toCpu(data)
+        toNumpy(data)
         return data
