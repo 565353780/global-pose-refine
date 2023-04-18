551f6504a69d5fa5ef1415ce025c143f8fb7f335
chLi <565353780@qq.com>
2023-04-18 18:59:33 +0800

    update pretrained model file path

--- global_pose_refine/Demo/trainer.py
+++ global_pose_refine/Demo/trainer.py
@@ -17,8 +17,7 @@ from global_pose_refine.Module.trainer import Trainer
 
 
 def demo():
-    model_file_path = './output/20230413_21:59:11/model_best.pth'
-    model_file_path = ''
+    model_file_path = './output/20230418_18:01:24/model_best.pth'
     resume_model_only = True
     print_progress = True
 
