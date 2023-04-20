71cb37ae7702c161a5ed53a454f93c03856ed00a
chLi <565353780@qq.com>
2023-04-20 22:28:44 +0800

    downgrade cuda version

--- dev_setup.sh
+++ dev_setup.sh
@@ -27,5 +27,5 @@ cd ../scannet-sim-manage
 
 pip install tqdm open3d numpy
 
-pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 \
-  --extra-index-url https://download.pytorch.org/whl/cu117
+pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 \
+  --extra-index-url https://download.pytorch.org/whl/cu116
